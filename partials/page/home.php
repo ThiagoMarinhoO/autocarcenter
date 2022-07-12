722c8a403fbbb3d4c88dfa7fa708413aa3e73a45
Thiago Marinho <travailmarinho@gmail.com>
2022-07-11 23:07:13 -0300

    2nd Commit

--- partials/page/home.php
+++ partials/page/home.php
@@ -2,6 +2,108 @@
 get_header();
 ?>
 
+    <section class="heroSection">
+        <header class="homeHeader">
+            <div class="superiorLogoHolder">
+                <img src="" alt="Logo autocarcenter">
+            </div>
+            <nav>
+                <ul class="navList">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Quem somos</a></li>
+                    <li><a href="">Contato</a></li>
+                    <li><a href="">Buscar</a></li>
+                </ul>
+            </nav>
+            <a href="http://localhost/autocarcenter/pagina-cadastro/" class="franqueadoBtn">Franqueamento</a>
+        </header>
+        <div class="heroBoxes">
+            <div class="blackBox">
+                <span class="chamada">Serviços automotivos para o seu dia a dia</span>
+            </div>
+            <div class="swiperBox">
+                <img src="" alt="" class="bannerPhoto">
+            </div>
+            <div class="redCharm">
+                <img src="" alt="" class="dots">
+            </div>
+        </div>
+        <div class="heroFilters">
+            <div class="nearMeSelect">
+                <label for="">Autocarcenter mais próxima</label>
+                <select id="estado" name="estado">
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                    <option value="EX">Estrangeiro</option>
+                </select>
+                <select id="estado" name="estado">
+                    <option value="" disabled="" selected="" hidden="">Selecione o tipo de experiência</option>
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                    <option value="EX">Estrangeiro</option>
+                </select>
+                <button><img src="" alt="Arrow pra filters"></button>
+            </div>
+        </div>
+        <div class="bestStores">
+            <div class="storeTitle">
+                <span>Auto Car Center</span>
+                <span>Penha, RJ</span>
+            </div>
+            <span class="storeLocation">Av. Lobo Júnior, 1287 - Penha Circular, Rio de Janeiro - RJ, 21020-122</span>
+
+        </div>
+    </section>
 
 
 <?php
